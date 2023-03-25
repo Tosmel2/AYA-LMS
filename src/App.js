8669ecda6d1b2e1f0cbf0ae5aa786a656df6430a
BLACKHENZ29 <ayehenz29@gmail.com>
2023-03-25 16:06:13 +0000

    admin login page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Homepage from './components/Homepage';
 import {  BrowserRouter, BrowserRouter as Router,
    Route,  Routes, useRoutes } from 'react-router-dom';
 import CoursePostingForm from './Page/Course/Courses';
+import AdminLogin from './Page/admin/adminLog';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={< Homepage/>} />
+        <Route exact path="/" element={< AdminLogin/>} />
         <Route  path="/course" element={<CoursePostingForm />} />
       </Routes>
       </BrowserRouter>
