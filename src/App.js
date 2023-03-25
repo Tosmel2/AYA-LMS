8c9cdcfe58598ef9a793507d29360fea99c9895e
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-03-25 23:25:50 +0100

    feat: Install tailwind in project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
-import { ChakraProvider, Switch, theme } from '@chakra-ui/react';
-import Homepage from './components/Homepage';
-import {  BrowserRouter, BrowserRouter as Router,
-   Route,  Routes, useRoutes } from 'react-router-dom';
-import CoursePostingForm from './Page/Course/Courses';
+import React from "react";
+import "./App.css";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import Homepage from "./components/Homepage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CoursePostingForm from "./Page/Course/Courses";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={< Homepage/>} />
-        <Route  path="/course" element={<CoursePostingForm />} />
-      </Routes>
+        <Routes>
+          <Route exact path='/' element={<Homepage />} />
+          <Route exact path='/course' element={<CoursePostingForm />} />
+          <Route exact path='/profile' element={<Profile />} />
+        </Routes>
       </BrowserRouter>
-      
     </ChakraProvider>
   );
 }
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='font-bold text-3xl text-center'>
+      Profile
+    </div>
+  )
+}
+
+export default Profile
